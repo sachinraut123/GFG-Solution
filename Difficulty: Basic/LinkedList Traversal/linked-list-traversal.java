89006a7b1a1d4918d673eb2efe5612ac5bf56de1
sachinraut123 <sachinraut13744@gmail.com>
2026-07-30 12:42:12 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/LinkedList Traversal/linked-list-traversal.java
@@ -0,0 +1,23 @@
+/*
+class Node {
+    int data;
+    Node next;
+
+    Node(int val) {
+        data = val;
+        next = null;
+    }
+}
+*/
+
+class Solution {
+    void printList(Node head) {
+        // code here
+        Node temp = head;
+        
+        while(temp!=null){
+            System.out.print(" "+temp.data);
+            temp = temp.next;
+        }
+    }
+}
